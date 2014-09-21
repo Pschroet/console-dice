866e8ff8564416af79ad2e0864cf114ae86b6b07
Pschroet <pschroet@inf.fu-berlin.de>
2014-09-21 19:42:21 +0200

    implemented addition of numbers by appending them with a plus sign

--- roll.rb
+++ roll.rb
@@ -1,10 +1,11 @@
 #roll a number of dice
-diceMuster = /[0-9]+d[0-9]+/							#a number of dice with number of sides
+diceMuster = /[0-9]+d[0-9]+([+][0-9]+)*/					#a number of dice with number of sides
 @useExplodingDice = false								#boolean to determine if the highest number triggers further rolls
 input = ""
 
 def diceRoll(input)
 	number, sides = input.to_s.split(/d/)				#get the number of the dice and the sides
+	sides, summand = sides.to_s.split(/[+]/)			#if there is a number to be added get that
 	number = number.to_i								#transform them into integers (either Fixnum or Bignum)
 	sides = sides.to_i
 	result = 0											#default value for the result so anything generated adds up
@@ -15,20 +16,25 @@ def diceRoll(input)
 		result += temp									#add it to the result so far
 		while(temp == sides && @useExplodingDice)		#if exploding dice is used and the highest number is rolled
 			temp = rand(sides) + 1						#the throw itself
-			rolls.push(temp)								#put the roll in the array
-			result += temp									#add it to the result so far
+			rolls.push(temp)							#put the roll in the array
+			result += temp								#add it to the result so far
 		end
 	end
-	puts "You rolled #{number}d#{sides}. Result is #{result} #{rolls}"
+	if(summand == nil)
+		puts "You rolled #{number}d#{sides}. Result is #{result} #{rolls}"
+	else
+		result += summand.to_i
+		puts "You rolled #{number}d#{sides}+" + summand + ". Result is #{result} #{rolls}"
+	end
 end
 
 #check the arguments
 for arg in ARGV
 	if arg == "-x"
 		then @useExplodingDice = true
-	elsif(arg.match(/[0-9]+d[0-9]+/))
+	elsif(arg.match(diceMuster))
 		if(input != "")
-			input = input + "+" + arg
+			input = input + "*" + arg
 		else
 			input = arg
 		end
@@ -38,8 +44,8 @@ end
 #roll the dice
 if nil==""
 	puts "Missing argument: [number]d[number][ [number]d[number] ...]*"
-elsif(input.match(/[0-9]+d[0-9]+([+][0-9]+d[0-9]+)*/))	#if there dices
-	dices = input.split("+")
+elsif(input.match(/[0-9]+d[0-9]+([+][0-9]+)*([+][0-9]+d[0-9]+([+][0-9]+)*)*/))	#if there dices
+	dices = input.split("*")
 	dices.each{
 		|dice|
 		diceRoll(dice)
